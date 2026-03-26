fc946f2a711ff731ee818b46c8254307cb53407f
kwong09 <wongkayla09@gmail.com>
2026-03-26 14:17:59 -0700

    turtle game animations (functions created)

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ var gameScreen = document.getElementById("gameScreen");
 var turtleGameScreen = document.getElementById("turtleGameScreen");
 var currentScreen = "start";
 
+//turtle game screen
+var talkingTurtlePerson = document.getElementById("talkingTurtlePerson");
+var talkingTurtlePersonBubble = document.getElementById("talkingTurtlePersonBubble");
+var talkingNextButton = document.querySelector(".talkingNextButton");
+var turtleGuessItemContainer = document.getElementById("turtleGuessItemContainer");
+var turtleGuessItem1 = document.getElementById("turtleGuessItem1");
+var turtleGuessItem2 = document.getElementById("turtleGuessItem2");
+var turtleGuessItem3 = document.getElementById("turtleGuessItem3");
+
 function updateScreen(screenValue) {
     currentScreen = screenValue;
     if (currentScreen == "gameMenu") {
@@ -20,3 +29,29 @@ function updateScreen(screenValue) {
     }
 }
 
+function reverseAnimation(object, animation) {
+    object.style.animation = "none";
+    object.offsetHeight;
+    object.style.animation = `${animation} 1s reverse forwards`;
+}
+
+function addAnimation(object, animation) {
+    object.style.animation = "none";
+    object.offsetHeight;
+    object.style.animation = `${animation} 1s forwards`;
+}
+
+function talkingTurtleGameNext(text) {
+    if (text == "talkingNext") {
+        reverseAnimation(talkingTurtlePerson, "talkingPersonUp");
+        reverseAnimation(talkingTurtlePersonBubble, "talkingBubbleUp");
+        reverseAnimation(talkingNextButton, "talkingNextButtonUp");
+
+        addAnimation(turtleGuessItemContainer, "turtleGuessItemContainerUp");
+    }
+}
+
+function turtleGuessItemChoice(option) {
+    reverseAnimation(turtleGuessItemContainer, "turtleGuessItemContainerUp");
+
+}
